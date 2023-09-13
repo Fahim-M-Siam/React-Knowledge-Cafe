8385ca71629bc5d77a1f9071e200ee9c275c7ddf
Fahime <fahim.mohammad.siam@gmail.com>
2023-09-14 00:50:04 +0600

    Set layout and Responsive outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
 // @ts-nocheck
 import Header from './Components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import './App.css'
 
 function App() {
@@ -8,7 +11,10 @@ function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import PropTypes from 'prop-types';
+
+
+const Blog = ({blog}) => {
+    console.log(blog)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
+}
+
+export default Blog;
